763a0827568a10a271fb4101a6c65648941eaa5d
Salvatore Stella <sstella@ncsu.edu>
2015-02-27 22:25:22 -0500

    Snapshot to go to work

---
+++ src/sage/combinat/tropical_cluster_algebra/__init__.py
@@ -0,0 +1,1 @@
+# Initialization file for tropical cluster algebras
---
+++ src/sage/combinat/tropical_cluster_algebra/all.py
@@ -0,0 +1,2 @@
+from sage.misc.lazy_import import lazy_import
+lazy_import("sage.combinat.tropical_cluster_algebra.tropical_cluster_algebra","TropicalClusterAlgebra")
