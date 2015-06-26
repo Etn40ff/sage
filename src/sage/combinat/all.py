235b53b47d2bf84b27e836126804bf03e1404e2a
Salvatore Stella <sstella@ncsu.edu>
2015-06-26 12:40:53 +0200

    Rebased on 6.8.beta6

--- src/sage/combinat/all.py
+++ src/sage/combinat/all.py
@@ -197,3 +197,5 @@ from binary_recurrence_sequences import BinaryRecurrenceSequence
 # Six Vertex Model
 lazy_import('sage.combinat.six_vertex_model', 'SixVertexModel')
 
+# Tropical model for cluster agebras
+from tropical_cluster_algebra.all import *
